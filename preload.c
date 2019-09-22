0a65165a46195068513817cce9f67ea7442c19a0
Henry Baba-Weiss <henry.babaweiss@gmail.com>
2019-09-22 12:48:53 -0700

    halve the frequency of the performance counter

--- preload.c
+++ preload.c
@@ -3,6 +3,18 @@
 #include <windows.h>
 #include <detours.h>
 
+static BOOL (WINAPI *Real_QueryPerformanceFrequency)(LARGE_INTEGER *lpFrequency)
+    = QueryPerformanceFrequency;
+
+static BOOL WINAPI Dilated_QueryPerformanceFrequency(LARGE_INTEGER *lpFrequency)
+{
+    if (!Real_QueryPerformanceFrequency(lpFrequency))
+        return FALSE;
+
+    lpFrequency->QuadPart >>= 1;
+    return TRUE;
+}
+
 BOOL APIENTRY DllMain(HINSTANCE hModule, DWORD dwReason, PVOID lpReserved)
 {
     (void)hModule;
@@ -13,6 +25,17 @@ BOOL APIENTRY DllMain(HINSTANCE hModule, DWORD dwReason, PVOID lpReserved)
 
     if (dwReason == DLL_PROCESS_ATTACH)
         DetourRestoreAfterWith();
+
+    DetourTransactionBegin();
+    DetourUpdateThread(GetCurrentThread());
+    if (dwReason == DLL_PROCESS_ATTACH) {
+        DetourAttach((PVOID *)&Real_QueryPerformanceFrequency,
+                     (PVOID)Dilated_QueryPerformanceFrequency);
+    } else if (dwReason == DLL_PROCESS_DETACH) {
+        DetourDetach((PVOID *)&Real_QueryPerformanceFrequency,
+                     (PVOID)Dilated_QueryPerformanceFrequency);
+    }
+    DetourTransactionCommit();
 
     return TRUE;
 }
